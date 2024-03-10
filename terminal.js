94f5650c0a2eb41f707a7b481b514c31e1e8f32c
Tammo Tammisto <Tammo.Tammisto@tptlive.ee>
2024-03-10 14:56:19 +0200

    tüübid valmis

--- terminal.js
+++ terminal.js
@@ -2,18 +2,77 @@ import inquirer from 'inquirer';
 
 inquirer
     .prompt([
-        { type: 'input', name: 'name', message: 'Whats your name?' },
-        { 
-            type: 'list',
-            name: 'food',
-            message: 'Whats your favorite food?',
-            choices: ['Pizza', 'Burger', 'Fries']
+        {
+            type: "checkbox",
+            name: "olendid",
+            message: "Vali endale meeldivad olendid",
+            choices: ["Kassid", "Konnad", "Linnud"]
         },
-        { 
-            type: 'checkbox',
-            name: 'foods',
-            message: 'What are your favorite foods?',
-            choices: ['Pizza', 'Burger', 'Fries']
+        {
+            type: "confirm",
+            name: "inimene",
+            message: "Kas sa olen inimene?"
+        },
+        {
+            type: "editor",
+            name: "fail",
+            message: "Kirjuta midagi"
+        },
+        {
+            type: "expand",
+            name: "tegemine",
+            message: 'Mida sa soovid peale kooli teha?',
+            default: 'j',
+            choices: [
+                {
+                    key: 'j',
+                    name: 'Juua?',
+                    value: 'minna jooma',
+                },
+                {
+                    key: 's',
+                    name: 'Magada?',
+                    value: 'minna magama',
+                },
+                {
+                    key: 'e',
+                    name: 'Hessi?',
+                    value: 'minna hessi',
+                },
+                {
+                    key: 'v',
+                    name: 'Mängima?',
+                    value: 'minna mängima',
+                },
+            ],
+        },
+        {
+            type: "input",
+            name: "nimi",
+            message: "Sisesta enda nimi"
+        },
+        {
+            type: 'list',
+            name: 'koduloom',
+            message: 'Eelistatud kodullom?',
+            choices: ['Koer', 'Kass', 'Laps']
+        },
+        {
+            type: "number",
+            name: "vanus",
+            message: "Sisesta enda vanus"
+        },
+        {
+            type: "password",
+            name: "parool",
+            message: "Sisesta parool!",
+
+        },
+        {
+            type: "rawlist",
+            name: "kumb",
+            message: "Vali!",
+            choices: ["Elu", "Surm"]
         },
     ])
     .then((answers) => {
